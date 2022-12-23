d4eee120972f341c7c778b3118b8b91bfef98e51
Erick Madrigal Flores <e.madrigal@cheilmex.local>
2022-12-23 09:42:43 -0600

    proyect AWS in react

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "Los31416two2": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
